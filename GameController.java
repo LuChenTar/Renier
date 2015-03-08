f50b5047c5adb885d966a3d58125f08a6e604c2c
Lu Chen <luchen@Lus-MacBook-Pro.local>
2015-03-08 19:55:54 +1100

    start module

---
+++ GameController.java
@@ -0,0 +1,11 @@
+package comp1140.ass2;
+
+import comp1140.ass2.gui.Board;
+
+public class GameController {
+
+    public static void main(String[] args) {
+		/* start the GUI which will start the game */
+        Board.start();
+    }
+}
